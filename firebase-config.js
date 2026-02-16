5981e20d07671e130a9342f78ed326912f7182d6
LizTimJulienAstrid CroLio <julienmlionnet@gmail.com>
2026-02-16 16:47:23 -0500

    redid the firebase-config to work with url

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDYLkGt8vFT2gfqNBKBZb26dlA0uxomZXw",
   authDomain: "lost-and-found-fbla-44b57.firebaseapp.com",
@@ -16,6 +14,6 @@ const firebaseConfig = {
   measurementId: "G-XMW9BM4FTX"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
