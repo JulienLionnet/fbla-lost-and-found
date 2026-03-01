ae76d8a1e2ccf0dbf1ecbe030f721aad84848eea
LizTimJulienAstrid CroLio <julienmlionnet@gmail.com>
2026-03-01 14:53:07 -0500

    adding itemlist extra features

--- login.js
+++ login.js
@@ -1,22 +1,24 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, signInwithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { firebaseConfig } from "./firebase-config.js";
+import { getAuth, signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const app = initializeApp({firebaseConfig});
-const auth = getAuth(app);
+import { db } from "./firebase-config.js";
+import { getApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+const auth = getAuth(getApp());
 
 const form = document.getElementById("loginForm");
 const errorMsg = document.getElementById("errorMsg");
 
-form.addEvemtListener("submit", async (e) => {
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    try {
-        await singInWithEmailAndPassword(auth, email, password);
-        window.location.href = "admin.html";
-    } catch (error) {
-        errorMsg.textContent = "Invalid login credentials.";
-    }
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "admin.html";
+  } catch (error) {
+    errorMsg.textContent = "Invalid login credentials.";
+  }
 });
